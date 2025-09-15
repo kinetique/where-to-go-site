2d396cb8d6b0a65d8d99689e11de70dd31f1abcb
kinetique <sonyacat0423@gmail.com>
2025-09-16 02:21:10 +0300

    Added changes in settings, made models, started to make views and templates for pages

--- places/views.py
+++ places/views.py
@@ -1,3 +1,26 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 
-# Create your views here.
+from places.models import Place
+
+
+def index(request):
+    place_ids = request.session.get('place_ids', [])
+    places = Place.objects.filter(id__in=place_ids)
+    selected_place = None
+    return render(request, 'places/index.html', {'selected_place': selected_place})
+
+
+def places_list(request):
+    place_ids = request.session.get('place_ids', [])
+    places = Place.objects.filter(id__in=place_ids).order_by('-created_at')
+    return render(request, 'places/places_list.html', {'places': places})
+
+
+def places_details(request):
+    place_ids = request.session.get('place_ids', [])
+    place = get_object_or_404(Place, id__in=place_ids)
+    return render(request, 'places/place_detail.html', {'place': place})
+
+
+def add_place(request):
+    return
